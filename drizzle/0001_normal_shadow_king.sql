3ca42b06deed6e05ba1fe48dc412c38c78a644e9
Nithish Gowda <niiithish@gmail.com>
2025-11-22 14:15:37 +0530

    fix: stop ignoring drizzle migration files

---
+++ drizzle/0001_normal_shadow_king.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "user" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "user" DROP COLUMN "timezone";--> statement-breakpoint
+ALTER TABLE "user" DROP COLUMN "language";--> statement-breakpoint
+ALTER TABLE "user" DROP COLUMN "use24Hour";
