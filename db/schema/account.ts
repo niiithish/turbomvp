bbab38eed29fa819ee7a2cca8649cd482eddc4c7
Nithish Gowda <niiithish@gmail.com>
2025-11-23 13:19:31 +0530

    feat: Implement Drizzle ORM, add user

--- db/schema/account.ts
+++ db/schema/account.ts
@@ -1,6 +1,5 @@
 import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
-import { relations } from "drizzle-orm";
 import { users } from "./users";
 
 export const account = pgTable("account", {
