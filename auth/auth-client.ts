4802f7042a661c9119d9c2c3c7c21ef7ee642812
Nithish Gowda <niiithish@gmail.com>
2025-11-25 14:54:24 +0530

    feat: add Dodo Payments client plugin to auth configuration

--- auth/auth-client.ts
+++ auth/auth-client.ts
@@ -1,9 +1,10 @@
 import { emailOTPClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { dodopaymentsClient } from "@dodopayments/better-auth";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL,
-  plugins: [emailOTPClient()],
+  plugins: [emailOTPClient(), dodopaymentsClient()],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
